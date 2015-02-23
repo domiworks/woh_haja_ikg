18188d47ae1dd31ed367b38a98a46775dbb28353
Gredy Prasetya <gredy.prasetya@gmail.com>
2015-02-23 12:29:18 +0700

    update datecnverter

--- app/controllers/ReportingController.php
+++ app/controllers/ReportingController.php
@@ -220,18 +220,18 @@ class ReportingController extends BaseController {
 	private function getMonthFromNumber($month){
 		$month = $month%12;
 		switch ($month) :
-			case  1: return 'Januari';
-			case  2: return 'Februari';
-			case  3: return 'Maret';
-			case  4: return 'April';
-			case  5: return 'Mei';
-			case  6: return 'Juni';
-			case  7: return 'Juli';
-			case  8: return 'Agustus';
-			case  9: return 'September';
-			case 10: return 'Oktober';
-			case 11: return 'November';
-			case 0: return 'Desember';
+			case  1: return 'JAN';
+			case  2: return 'FEB';
+			case  3: return 'MAR';
+			case  4: return 'APR';
+			case  5: return 'MEI';
+			case  6: return 'JUN';
+			case  7: return 'JUL';
+			case  8: return 'AGU';
+			case  9: return 'SEP';
+			case 10: return 'OKT';
+			case 11: return 'NOV';
+			case 0: return 'DES';
 				
 		endswitch;
 	}
